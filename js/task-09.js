e599a2cd2a96301587350295ab7815b3865516cd
Vova-fil <vfil.zp@gmail.com>
2021-12-08 23:07:15 +0200

    задание 8 и 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const colorEl = document.querySelector('button.change-color');
+let colorElselect = document.querySelector('span.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onColorSelect = (e) => (colorElselect.textContent = getRandomHexColor());
+
+colorEl.addEventListener('click', onColorSelect);
+
+console.dir(colorElselect);

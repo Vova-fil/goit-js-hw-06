81d305a41d55ef63f558095477b03640c71e13b1
Vova-fil <vfil.zp@gmail.com>
2021-12-09 00:22:50 +0200

    исправил 9 задание

--- js/task-09.js
+++ js/task-09.js
@@ -5,8 +5,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const onColorSelect = (e) => (colorElselect.textContent = getRandomHexColor());
+const onColorSelect = () => {
+  colorElselect.textContent = getRandomHexColor();
+
+  document.body.style.backgroundColor = colorElselect.textContent;
+};
+
+//color.style.backgroundColor = colorElselect.textContent
 
 colorEl.addEventListener('click', onColorSelect);
 
 console.dir(colorElselect);
+//console.log();
